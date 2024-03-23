59c9b35963f4e387e453cb9fb99ee793536c8283
Donaji Coello <donnsdonns14@gmail.com>
2024-03-22 21:55:55 -0600

    tarea1

---
+++ adivinaMiNumero.js
@@ -0,0 +1,24 @@
+const valorInicial = 1;
+const valorFinal = 100;
+const numeroInicialUsuario = 50;
+
+const numeromas = document.querySelector("#butonMas")
+
+// function busquedaBinaria(valorInicial, valorFinal){
+//     numero=Math.floor( (valorFinal-valorInicial)/2 )
+
+
+
+//     if (numero < valorFinal)   
+
+//     else 
+//     document.querySelector(butonMenos) 
+//     valorInicial=50 valorFinal=100
+
+// }
+numeromas.addEventListener("click", (evento) => 
+ {  valorInicial   });
+
+
+console.log(numeromas);
+const numeromenos = document.querySelector("#butonMenos")

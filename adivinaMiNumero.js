666ec864ae6d52250db1d6a64ddd73c05224e5fa
Donaji Coello <donnsdonns14@gmail.com>
2024-03-22 23:22:22 -0600

    estilos

--- adivinaMiNumero.js
+++ adivinaMiNumero.js
@@ -1,24 +1,56 @@
-const valorInicial = 1;
-const valorFinal = 100;
-const numeroInicialUsuario = 50;
+let valorInicial = 1;
+let valorFinal = 100;
+let numeroInicialUsuario = 50;
+
+
 
 const numeromas = document.querySelector("#butonMas")
-
-// function busquedaBinaria(valorInicial, valorFinal){
-//     numero=Math.floor( (valorFinal-valorInicial)/2 )
-
+const numeromenos = document.querySelector("#butonMenos")
+const numeroAcertado = document.querySelector("#botonGanador")
 
 
-//     if (numero < valorFinal)   
 
-//     else 
-//     document.querySelector(butonMenos) 
-//     valorInicial=50 valorFinal=100
+function busquedaBinaria(){
+    const numero = Math.floor( (valorFinal+valorInicial)/2 )
+    document.getElementById("numeroMostrado").innerHTML = numero;
+    console.log("el numero es?: "+numero);
+}
+numeromas.addEventListener("click", () => {  
 
-// }
-numeromas.addEventListener("click", (evento) => 
- {  valorInicial   });
+    console.log("========= se oprimio es mayor ============")
+    valorInicial = Math.floor((valorFinal+valorInicial)/2)
+    console.log("valor inicial: ",valorInicial); 
+    console.log("valor final ",valorFinal); 
+    busquedaBinaria()
 
+ });
+
+ numeromenos.addEventListener("click", () => {  
+
+    console.log("========= se oprimio es menor ============")
+    console.log("valor inicial: ",valorInicial); 
+    valorFinal = Math.floor( (valorInicial+valorFinal)/2 )
+    console.log("valor final: ",valorFinal); 
+    busquedaBinaria();
+
+ });
 
 console.log(numeromas);
-const numeromenos = document.querySelector("#butonMenos")
+
+
+numeroAcertado.addEventListener("click", () => {
+    document.getElementById("ganador").innerHTML="¡Se ha adivinado tu número!"
+}); 
+
+
+
+
+// num ---> 50
+// num > 50 ----> 75
+// 50 -- > val inicial y 75 --> val final
+// es menor --> 12 (val final)
+//   menor -- 50 a val final --> x < 75
+// x - 75 = 50
+// x = 75 + 50 // valor total
+
+
